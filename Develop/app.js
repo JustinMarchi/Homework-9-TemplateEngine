f1fb78307cd2ca88ba8adff6eaf3518734ad47b1
Justin Marchi <marchij86@gmail.com>
2020-04-14 01:23:49 -0400

    Deleted hidden Unicode characters. Added Inquirer questions including functionality of Role dependent questions.

--- Develop/app.js
+++ Develop/app.js
@@ -4,30 +4,87 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
-​
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your ID Number?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your e-mail address?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is your role in the team?",
+        choices: [{
+            name: "Manager"
+        },
+        {
+            name: "Engineer"
+        },
+        {
+            name: "Intern"
+        }]
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your office number?",
+        when: function(answers){
+            return answers.role === "Manager";
+        }
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+        when: function(answers){
+            return answers.role === "Engineer";
+        }
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is the name of the school you're currently attending?",
+        when: function(answers){
+            return answers.role === "Intern";
+        }
+    }
+];
+function promptUser() {
+
+   return inquirer.prompt(questions);
+};
+
+promptUser();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
